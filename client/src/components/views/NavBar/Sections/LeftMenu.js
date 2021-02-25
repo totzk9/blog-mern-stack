8e5f47b3da2668ec70ae459aa25117adb3db410d
Tyrone Chris Abad <tyronechris67@gmail.com>
2021-02-25 15:08:50 +0800

    commit

---
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Menu } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+function LeftMenu(props) {
+  return (
+    <Menu mode={props.mode}>
+    <Menu.Item key="mail">
+      <a href="/">Home</a>
+    </Menu.Item>
+    <Menu.Item key="mail">
+      <a href="/blog">Blog</a>
+    </Menu.Item>
+    <Menu.Item key="mail">
+      <a href="/blog/create">CreatePage</a>
+    </Menu.Item>
+  </Menu>
+  )
+}
+
+export default LeftMenu
---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:'mongodb+srv://tribox-tyrone:Za2AnzENaMM7dgQM@triboxwebsite.f9cmw.mongodb.net/test?retryWrites=true&w=majority'
+}
